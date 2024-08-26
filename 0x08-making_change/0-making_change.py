50b025b26835393ce0cd768fbc983213a9b7f636
AcademiccMacharia <machariab19@gmail.com>
2024-08-26 04:18:32 -0700

    [fix]: Fixing making change execcution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+"""
+
 
 def makeChange(coins, total):
     """
-    Determines the fewest number of coins needed to meet a given total.
+    Determines the fewest coins to meet total
 
-    :param coins: List of integers representing coin denominations.
-    :param total: Integer representing the total amount to be made.
-    :return: Fewest number of coins needed to meet the total. Returns -1 if not possible.
+    Args:
+        coins (list): list of coins
+        total (int): value to meet as total
+
+    Returns:
+        coins (int): fewest number of coins needed to meet total
+        (0) if total is 0 or less
+        (-1) if no number of the coins can meet the total
     """
     if total <= 0:
         return 0
-    
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            if dp[amount - coin] != float('inf'):
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+    min_coins = [0] + [float('inf')] * total
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                num_coins = min_coins[i - coin] + 1
+                if num_coins < min_coins[i]:
+                    min_coins[i] = num_coins
+    if min_coins[-1] == float("inf"):
+        return -1
+    return min_coins[total]
